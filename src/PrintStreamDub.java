5c8d12bfcf88a80110b25ffc2b9b7ffcfb914c7c
me <saveliev@dlit.dp.ua>
2017-09-02 19:13:00 +0300

    next v.

--- src/PrintStreamDub.java
+++ src/PrintStreamDub.java
@@ -1,8 +1,8 @@
+import java.io.FilterOutputStream;
 import java.io.OutputStream;
-import java.io.PrintStream;
 import java.io.Serializable;
 
-public class PrintStreamDub extends PrintStream implements Serializable {
+public class PrintStreamDub extends FilterOutputStream implements Serializable {
     public PrintStreamDub(OutputStream out) {
         super(out);
     }
