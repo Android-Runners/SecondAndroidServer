4669c1e1e722c84c5b000feb7ebb8e60cac547b2
Daniil Muntyan <daniilmuntjan@gmail.com>
2017-11-29 10:24:23 +0200

    РАБОТАЕТ!!! Окончательная версия 1

--- src/Client.java
+++ src/Client.java
@@ -16,6 +16,10 @@ public class Client implements Runnable {
 
     private Socket socket;
 
+    public int getNumber() {
+        return number;
+    }
+
     private int number;
 
     public ObjectOutputStream getOutput() {
@@ -56,21 +60,28 @@ public class Client implements Runnable {
                     int type = (int) input.readObject();
                     if(type == -1) {
                         isSharing = true;
-                        Object object = input.readObject();
-                        if(object.equals(-2)) {
-                            for(HashMap.Entry<Integer, Client> client : clients.entrySet()) {
-                                client.getValue().getOutput().writeObject(-2);
+                        while(isSharing) {
+                            Object object = input.readObject();
+                            System.out.println(((byte[])object).length);
+                            if(object.equals(-2)) {
+                                for(HashMap.Entry<Integer, Client> client : clients.entrySet()) {
+                                    client.getValue().getOutput().writeObject(-2);
+                                }
+                                isSharing = false;
                             }
-                        }
-                        else {
-                            for(HashMap.Entry<Integer, Client> client : clients.entrySet()) {
-                                client.getValue().getOutput().writeObject(object);
+                            else {
+                                for(HashMap.Entry<Integer, Client> client : clients.entrySet()) {
+                                    client.getValue().getOutput().writeObject(object);
+                                    System.out.println("Client #" + number + " sends client #" + client.getValue().getNumber());
+                                }
                             }
                         }
                     }
                     else if(type == -3) {
                         int i = (int) input.readObject();
-                        server.getClients().get(i).getClients().put(i, this);
+                        System.out.println("!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!! " + i);
+                        server.getClients().get(i).getClients().put(number, this);
+                        System.out.println(server.getClients().get(i).getClients().size());
                     }
                     else if(type == -4) {
                         int i = (int) input.readObject();
