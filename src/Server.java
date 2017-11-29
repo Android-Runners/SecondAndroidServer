e08dff8a81a6ef643a0761056c5efe3037402ae8
me <saveliev@dlit.dp.ua>
2017-11-29 08:17:37 +0200

    VLAD

--- src/Server.java
+++ src/Server.java
@@ -1,8 +1,4 @@
-import com.example.androidclient.Message;
-
 import java.io.IOException;
-import java.io.ObjectOutputStream;
-import java.net.InetAddress;
 import java.net.ServerSocket;
 import java.net.Socket;
 import java.util.LinkedList;
@@ -41,47 +37,6 @@ public class Server implements Runnable {
             } catch (IOException e) {
                 e.printStackTrace();
             }
-        }
-    }
-
-    public void sendToAllUsers(Message message, int fromClientNumber) {
-
-        message.setIdSender(fromClientNumber);
-
-        for(Client client : clients) {
-            try {
-                if(fromClientNumber != -1 && client == clients.get(fromClientNumber)) {
-                    continue;
-                }
-                ObjectOutputStream output = client.getOutput();
-                output.writeObject(message);
-                output.flush();
-            } catch (IOException e) {
-                e.printStackTrace();
-            }
-        }
-    }
-
-    void sendMessage(Message message, int fromClientNumber) {
-        try {
-
-            System.out.println("Receiver " + message.getIdReceiver());
-
-            message.setIdSender(fromClientNumber);
-
-            if (message.getIdReceiver() == -1) {
-                sendToAllUsers(message, fromClientNumber);
-                return;
-            }
-
-            ObjectOutputStream output = clients.get(message.getIdReceiver()).getOutput();
-            output.flush();
-            output.writeObject(message);
-            output.flush();
-
-            System.out.println("Client #" + fromClientNumber + " sent client #" + message.getIdReceiver() + " " + message.getData());
-        } catch (IOException e) {
-            e.printStackTrace();
         }
     }
 }
